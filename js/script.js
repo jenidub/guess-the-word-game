60af35e94053c8ea29f69f169424a9acb23ea5bc
jeni_dub <stanfordjeni@gmail.com>
2022-02-15 22:33:08 -0800

    Added random word list async fetch functionality and tracking remaining guesses functionality to the Guess the Word web game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/***** GLOBAL VARIABLES LIST *****/
+/***** GLOBAL VALUES LIST *****/
 //Store the player's guessed letters in the game (unordered list element)
-const guessedLetters = document.querySelector(".guessed-letters")
+const guessedLetters = document.querySelector(".guessed-letters");
 
 //Guess button selector
 const guessButton = document.querySelector(".guess");
@@ -24,21 +24,39 @@ const playAgainButton = document.querySelector(".play-again");
 //Maintain an array of guessed letters
 const guesses = [];
 
-//Test word variable
-let word = "magnolia";
+//Counts the remaining guesses
+let numGuesses = 8;
+
+//Store the word for the game
+let word = "actually";
+let playerWord = "";
+
+//Retrieve word list to select from txt word list on Skillcrush
+/*const getWords = async function () {
+    const data = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await data.text();
+    const wordArray = words.split("\n");
+    const index = Math.floor(Math.random() * wordArray.length);
+    word = wordArray[index].trim();
+    console.log(word);
+    updateWordInProgress(word);
+}
+
+//Start the game!
+getWords();
+*/
 
 /***** MAIN JS SCRIPT *****/
 
-//Create function to create an array of dots to represent the target word
+//Create an array of dots to represent the target word
 const updateWordInProgress = function (string) {
     let arr = [];
     for (let letter of string) {
         arr.push("●");
     };
+    playerWord = arr.join("");
     console.log(arr.join(""));
-};
-
-updateWordInProgress(word);
+}
 
 //Validate user input as a single letter
 const validateInput = function(input) {
@@ -60,10 +78,11 @@ const makeGuess = function(letter) {
     letter = letter.toUpperCase();
 
     if (guesses.includes(letter)) {
-        messages.innerText = "You have already guessed this letter. Please try another letter!";
+        message.innerText = "You have already guessed this letter. Please try another letter!";
     } else {
         guesses.push(letter);
         updateLetterDisplay();
+        updateGuesses(letter);
         wordInProgress(guesses);
     };
 }
@@ -91,15 +110,35 @@ const wordInProgress = function(arr) {
         }
     };
 
+    playerWord = matches.join("");
     wordProgress.innerText = matches.join("");
+}
+
+//Update the number of guesses
+const updateGuesses = function (guess) {
+    word = word.toUpperCase();
+    console.log(word, guess);
+
+    if (word.includes(guess)) {
+        message.innerText = "Congratulations! You guessed correctly. Select another letter.";
+    } else {
+        numGuesses -= 1;
+        remainingSpan.innerText = `${numGuesses} guesses`;
+        message.innerText = "Sorry that letter was not in the word. Please try again. You'll get it next time!";
+    }
+
+    if (numGuesses === 1) {
+        message.innerText = `Be careful player. You only have one guess remaining. Don't screw up!`;
+    } else if (numGuesses === 0) {
+        message.innerText = `GAME OVER! You used all of your guesses. The word was ${word}.`;
+    }
 }
 
 //Check if the player won
 const playerWin = function() {
-    let playerGuess = guesses.join("");
-    console.log(playerGuess, word);
+    console.log(playerWord, word);
 
-    if (playerGuess === word) {
+    if (playerWord === word) {
         message.classList.add("win");
         message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>.';
     }
@@ -113,12 +152,11 @@ guessButton.addEventListener("click", function(e) {
     let userInput = playerInput.value;
     playerInput.value = "";
     let validInput = validateInput(userInput);
-    console.log(validInput);
     if (validInput !== undefined) {
         makeGuess(validInput);
     };
     wordInProgress(guesses);
     playerWin();
-});
+})
 
 //**NOTES: What do you do if the ltter has more than one copy of a letter? */
