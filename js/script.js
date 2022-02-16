2ebad7b8c494b9f390732d7f1251cdda44be98be
jeni_dub <stanfordjeni@gmail.com>
2022-02-15 19:57:23 -0800

    Added accepting and validating player guesses functionality to the Guess the Word web game

--- js/script.js
+++ js/script.js
@@ -16,10 +16,13 @@ const remainingGuess = document.querySelector(".remaining");
 const remainingSpan = document.querySelector(".remaining span");
 
 //Game messages paragraph selector
-const messages = document.querySelector(".message");
+const message = document.querySelector(".message");
 
 //Play again button selector
 const playAgainButton = document.querySelector(".play-again");
+
+//Maintain an array of guessed letters
+const guesses = [];
 
 //Test word variable
 let word = "magnolia";
@@ -37,10 +40,41 @@ const updateWordInProgress = function (string) {
 
 updateWordInProgress(word);
 
+//Validate user input as a single letter
+const validateInput = function(input) {
+    const acceptedLetter = /[a-zA-Z]/;
+
+    if (input === "") {
+        message.innerText = "Please enter a letter A-Z to play the game";
+    } else if (input.length > 1) {
+        message.innerText = "You entered more than one letter. Please enter a single letter A-Z" 
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "You entered an invalid character. Please enter a letter A-Z to play the game"
+    } else {
+        return input;
+    }
+}
+
+//
+const makeGuess = function(letter) {
+    if (guesses.includes(letter.toUpperCase())) {
+        messages.innerText = "You have already guessed this letter. Please try another letter!";
+    } else {
+        guesses.push(letter);
+    }
+    console.log(guesses);
+}
+
+
 //Create function to handle when the player clicks the Guess button
 guessButton.addEventListener("click", function(e) {
+    message.value = "";
     e.preventDefault();
     let userInput = playerInput.value;
-    console.log(userInput);
     playerInput.value = "";
+    let validInput = validateInput(userInput);
+    console.log(validInput);
+    if (validInput !== undefined) {
+        makeGuess(validInput);
+    };
 });
